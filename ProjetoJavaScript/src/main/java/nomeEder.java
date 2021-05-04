f3c8e78ba8f506d7b32f2cf7d8090ccddf685610
ederSilvaDev <81658662+ederSilvaDev@users.noreply.github.com>
2021-05-03 22:08:33 -0300

    Projeto java Script

---
+++ ProjetoJavaScript/src/main/java/nomeEder.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class nomeEder {
+	
+	public static void main(String[] args) {
+		
+		String nome = "Éder da Silva";
+		
+		JOptionPane.showMessageDialog(null, "Meu nome é = " + nome);
+		
+	}
+
+}
